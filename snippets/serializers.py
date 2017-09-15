a8d121074136337fdef20d4b830d090c7b71f652
jakobpederson <jakob.pederson+github@gmail.com>
2017-09-15 13:22:38 -0500

    Using ModelSerializers

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,40 +1,27 @@
-# Creating a Serializer class
-# The first thing we need to get started on our Web API is to provide a way of serializing and deserializing the snippet
-# instances into representations such as json. We can do this by declaring serializers that work very similar to Djangos
-# forms. Create a file in the snippets directory named serializers.py and add the following.
+# Using ModelSerializers
+# Our SnippetSerializer class is replicating a lot of information that's also contained in the Snippet model. It would be
+# nice if we could keep our code a bit more concise.
+
+# In the same way that Django provides both Form classes and ModelForm classes, REST framework includes both Serializer
+# classes, and ModelSerializer classes.
+
+# Let's look at refactoring our serializer using the ModelSerializer class. Open the file snippets/serializers.py again,
+# and replace the SnippetSerializer class with the following.
 
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+# One nice property that serializers have is that you can inspect all the fields in a serializer instance, by printing
+# its representation. Open the Django shell with python manage.py shell, then try the following:
 
-#  The first part of the serializer class defines the fields that get serialized/deserialized. The create() and
-#  update() methods define how fully fledged instances are created or modified when calling serializer.save()
-#  A serializer class is very similar to a Django Form class, and includes similar validation flags on the various
-#  fields, such as required, max_length and default.
-
-#  The field flags can also control how the serializer should be displayed in certain circumstances, such as when
-#  rendering to HTML. The {'base_template': 'textarea.html'} flag above is equivalent to using widget=widgets.Textarea
-#  on a Django Form class. This is particularly useful for controlling how the browsable API should be displayed, as
-
-#  we'll see later in the tutorial.
-#  We can actually also save ourselves some time by using the ModelSerializer class, as we'll see later, but for now
-#  we'll keep our serializer definition explicit.
+# It's important to remember that ModelSerializer classes don't do anything particularly magical, they are simply a
+# shortcut for creating serializer classes:
+#     * An automatically determined set of fields.
+#     * Simple default implementations for the create() and update() methods.
